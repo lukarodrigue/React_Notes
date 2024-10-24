52d25108991090ad6458068db845ff506c3fdef6
luka <lukarodrigues25@gmail.com>
2024-10-23 22:27:18 -0300

    PageHome_Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global'
-import theme from './styles/theme'
-import  { Details }  from './pages/Details'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+import  { Home }  from './pages/Home';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Details></Details>
+    <ThemeProvider theme={ theme }>
+      <Home></Home>
       <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,10 @@
-import { Container, Links } from "./styles.js"
+import { Container, Links, Content } from "./styles.js"
 
 import { Header } from "../../components/Header/index.jsx"
 import { Button } from "../../components/Button/index.jsx"
 import { Section } from "../../components/Section/index.jsx"
+import { ButtonText } from "../../components/ButtonText/index.jsx"
+import { Tag } from "../../components/Tag//index.jsx"
 
 
 
@@ -11,20 +13,42 @@ export function Details() {
     <Container>
       <Header> </Header>
       
-      <Section title="Links úteis">
-          <Links> 
-            <li>
-              <a href="#">https://www.rocketseat.com.br/</a>
-            </li>
-            <li>
-              <a href="#">https://www.rocketseat.com.br/</a>
-            </li>
-          </Links> 
-      </Section>
-      
-      
-      <Button title="Voltar" ></Button>
+      <main>
+        <Content>
+          <ButtonText title='Excluir nota'>
+            
+          </ButtonText>
+            <h1>
+            Introdução ao React
+            </h1>
+
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+            </p>
+
+          <Section title="Links úteis">
+              <Links> 
+                <li>
+                  <a href="#">https://www.rocketseat.com.br/</a>
+                </li>
+                <li>
+                  <a href="#">https://www.rocketseat.com.br/</a>
+                </li>
+              </Links> 
+          </Section>
+          
+          <Section title="Marcadores">
+              <Tag title="express"></Tag>
+              <Tag title="node"></Tag>
+          </Section>
+
+          <Button title="Voltar" ></Button>
+
+       </Content>
+      </main>
 
     </Container>
   )
 }
+
